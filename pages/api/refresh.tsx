abc099b27286ec6fbf2a853be6ced495ea8a1181
Yacinn <sassou.est100@gmail.com>
2022-09-24 10:36:09 +0100

    fixes

--- pages/api/refresh.tsx
+++ pages/api/refresh.tsx
@@ -16,6 +16,6 @@ export default async function refresh (req:NextApiRequest, res:NextApiResponse){
             return res.status(200).json({userInfo: userInfo, currentTrack: "None"});
         }
         const lyrics = await GetLyrics(currentTrack.item.artists[0].name, currentTrack.item.name);
-        return res.status(200).json({userInfo: userInfo, currentTrack: currentTrack});
+        return res.status(200).json({userInfo: userInfo, currentTrack: currentTrack, lyrics: lyrics});
     }
 }
