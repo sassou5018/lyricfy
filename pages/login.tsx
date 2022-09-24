28a0bd2ce001023f5b96617b9c9153b8f8655261
Yacinn <sassou.est100@gmail.com>
2022-09-24 04:41:12 +0100

    Added currently playing scope

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,7 +13,7 @@ export async function getServerSideProps() {
     const redirect_uri = process.env.REDIRECT_URI;
     return {
         redirect: {
-            destination: `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=user-read-private%20user-read-email&redirect_uri=${redirect_uri}&state=5018`,
+            destination: `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=user-read-private%20user-read-email%20user-read-currently-playing&redirect_uri=${redirect_uri}&state=5018`,
         }
     }
 }
