713709be28b806a91c8f4a9aac98bf0d08a6b288
Yacinn <sassou.est100@gmail.com>
2023-01-07 23:41:16 +0100

    updated to use V2 of the genius API

--- utils/geniusApi.ts
+++ utils/geniusApi.ts
@@ -1,21 +1,28 @@
 async function SearchSong(title: string, artist: string) {
-    const url = `https://genius-song-lyrics1.p.rapidapi.com/search?q=${title.replace(
-        /\s+/g,
+    // const url = `https://genius-song-lyrics1.p.rapidapi.com/search?q=${title.replace(
+    //     /\s+/g,
+    //     '%20'
+    // )}%20${artist.replace(/\s+/g, '%20')}&per_page=3&page=1`
+    const url = `https://genius-song-lyrics1.p.rapidapi.com/search/?q=${title.replace(
+        ' ',
         '%20'
-    )}%20${artist.replace(/\s+/g, '%20')}&per_page=3&page=1`
+    )}%20${artist.replace(' ', '%20')}&per_page=10&page=1`
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com',
         },
     }
     //@ts-ignore
     const result = await fetch(url, options)
     const data = await result.json()
-    if (data.meta.status == 200) {
+    if (result.status == 200) {
+        console.log('lyrics Data Search: ', data.hits[0].result)
         return data
     } else {
+        console.log('Lyrics Search error:', data)
+        console.log('status Code:', result.status)
         return { error: 'No song found' }
     }
 }
@@ -25,21 +32,28 @@ export default async function GetLyrics(title: string, artist: string) {
     if (id.error) {
         return id
     }
-    const url = `https://genius-song-lyrics1.p.rapidapi.com/songs/${id.response.hits[0].result.id}/lyrics`
+    //const url = `https://genius-song-lyrics1.p.rapidapi.com/song/lyrics?id=${id.hits[0].result.id}`
+    const url = `https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/?id=${id.hits[0].result.id}&text_format=plain`
     const options = {
         method: 'GET',
         headers: {
-            'x-rapidapi-key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com',
         },
     }
     //@ts-ignore
     const result = await fetch(url, options)
     const data = await result.json()
-    if (data.meta.status == 200) {
+    if (result.status == 200) {
+        console.log('lyrics Data:', data.lyrics.lyrics)
         return {
-            lyrics: data.response.lyrics,
-            metadata: id.response.hits[0].result,
+            lyrics: data.lyrics,
+            metadata: id.hits[0].result,
         }
-    } else return { error: 'No lyrics found' }
+    } else {
+        console.log('Lyrics error:', data)
+        return {
+            error: 'No lyrics found',
+        }
+    }
 }
